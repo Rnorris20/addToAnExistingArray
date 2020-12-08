b9ccc99016d72d16509c413bc0abdb755ee950df
RNorris6 <ryan.norris@patelhs.org>
2020-12-08 03:31:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    String[] msBenny = new String[10];
+    String[] mrBarry = new String[10];
+
+    Scanner scan = new Scanner(System.in);
+    for(int i = 0; i < barry.length; i++)
+    {
+      System.out.println("Hello student, What is your name?");
+      barry[i] = scan.nextLine(); 
+
+
+
+  }
+}
