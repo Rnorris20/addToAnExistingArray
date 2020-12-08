4019309c4f7a23939c3db939ce5b3bdff9aa1c39
RNorris6 <ryan.norris@patelhs.org>
2020-12-08 03:42:58 +0000

    Finished the arrays and created an alternate print statement for the last 2 students.

--- Main.java
+++ Main.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     String[] msBenny = new String[10];
     String[] mrBarry = new String[10];
 
     Scanner scan = new Scanner(System.in);
-    for(int i = 0; i < barry.length; i++)
+    for(int i = 0; i < mrBarry.length; i++)
     {
       System.out.println("Hello student, What is your name?");
-      barry[i] = scan.nextLine(); 
-      
+      mrBarry[i] = scan.nextLine(); 
+    }
 
-
+    for (int i = 0; i < mrBarry.length; i++)
+    {
+      //establishes a connection in order to transfer the values from one array to another.
+      msBenny[i] = mrBarry[i];
+      System.out.println(msBenny[i]);
+      // print statement to create a new array roster.
+    }
+    msBenny[11] = "Marco";
+    //created a statement to establish the 11th variable
+    msBenny[12] = "John";
+    //created a statement to establish the 12th variable
+      System.out.println("After these ten students, comes students number 11 and 12, named: " + msBenny[11] + "and" + msBenny[12]);
+      //print statement to show the 2 new students.
 
   }
 }
