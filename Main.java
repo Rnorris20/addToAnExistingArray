712298db8ce676971e20c4603dec7cc85127e736
RNorris6 <ryan.norris@patelhs.org>
2020-12-08 03:44:07 +0000

    Created comments for the code.

--- Main.java
+++ Main.java
@@ -4,14 +4,14 @@ class Main {
   public static void main(String[] args) {
     String[] msBenny = new String[10];
     String[] mrBarry = new String[10];
-
+    // establishes the 2 string statments for the seperate classes
     Scanner scan = new Scanner(System.in);
     for(int i = 0; i < mrBarry.length; i++)
     {
       System.out.println("Hello student, What is your name?");
       mrBarry[i] = scan.nextLine(); 
     }
-
+    // Scan statement for the input on the students names.
     for (int i = 0; i < mrBarry.length; i++)
     {
       //establishes a connection in order to transfer the values from one array to another.
